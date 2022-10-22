d18af62d32b1015a096149cf3f1ffe0225c7cb06
mikseros <mmadynski@gmail.com>
2022-10-22 18:30:01 +0200

    First test method to create first Role in DB

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -14,12 +14,25 @@ public class Role {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@Column(length = 40, nullable = false, unique = true)
 	private String name;
-	
+
 	@Column(length = 150, nullable = false)
 	private String description;
+
+	public Role(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	public Role(String name) {
+		this.name = name;
+	}
+
+	public Role() {
+
+	}
 
 	public Integer getId() {
 		return id;
