dd33843b9f57ae82e6aadadf3c18696fc153fd7d
mikseros <mmadynski@gmail.com>
2022-10-23 20:54:42 +0200

    Test Create New User with One Role

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -1,11 +1,17 @@
 package com.shopme.admin.user;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
@@ -15,8 +21,17 @@ public class UserRepositoryTests {
 	@Autowired
 	private UserRepository repo;
 	
+	@Autowired
+	private TestEntityManager entityManager;
+	
 	@Test
 	public void testCreateUser() {
+		Role roleAdmin = entityManager.find(Role.class, 1);
+		User userMikseros = new User("mmadynski@gmail.com", "123456", "Mikseros", "Mikseros");
+		userMikseros.addRole(roleAdmin);
 		
+		User savedUser = repo.save(userMikseros);
+		
+		assertThat(savedUser.getId()).isGreaterThan(0);
 	}
 }
