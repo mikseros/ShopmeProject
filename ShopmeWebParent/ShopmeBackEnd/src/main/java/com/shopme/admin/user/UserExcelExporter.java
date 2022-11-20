77547a29b94c67697eafc9fcc3dcc6db11651bfe
mikseros <mmadynski@gmail.com>
2022-11-20 20:18:50 +0100

    Export to Excel -> writing the data

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserExcelExporter.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserExcelExporter.java
@@ -44,6 +44,7 @@ public class UserExcelExporter extends AbstractExporter {
 	
 	private void createCell(XSSFRow row, int columnIndex, Object value, CellStyle style) {
 		XSSFCell cell = row.createCell(columnIndex);
+		sheet.autoSizeColumn(columnIndex);
 		
 		if (value instanceof Integer) {
 			cell.setCellValue((Integer) value);
@@ -61,10 +62,32 @@ public class UserExcelExporter extends AbstractExporter {
 		super.setResponseHeader(response, "application/octet-stream", ".xlsx");
 		
 		writeHeaderLine();
+		writeDataLines(listUsers);
 		
 		ServletOutputStream outputStream = response.getOutputStream();
 		workbook.write(outputStream);
 		workbook.close();
 		outputStream.close();
+	}
+	
+	private void writeDataLines(List<User> listUsers) {
+		int rowIndex = 1;
+		
+		XSSFCellStyle cellStyle = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setFontHeight(14);
+		cellStyle.setFont(font);
+		
+		for (User user : listUsers) {
+			XSSFRow row = sheet.createRow(rowIndex++);
+			int columnIndex = 0;
+			
+			createCell(row, columnIndex++, user.getId(), cellStyle);
+			createCell(row, columnIndex++, user.getEmail(), cellStyle);
+			createCell(row, columnIndex++, user.getFirstName(), cellStyle);
+			createCell(row, columnIndex++, user.getLastName(), cellStyle);
+			createCell(row, columnIndex++, user.getRoles().toString(), cellStyle);
+			createCell(row, columnIndex++, user.isEnabled(), cellStyle);
+		}
 	}
 }
