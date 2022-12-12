6e1ed64067e44699ac42a2e3de656e7236b3eb02
mikseros <mmadynski@gmail.com>
2022-12-12 22:41:09 +0100

    updateDetails handler method for updating user acc. det.

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/AccountController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/AccountController.java
@@ -1,11 +1,19 @@
 package com.shopme.admin.user;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.admin.FileUploadUtil;
 import com.shopme.admin.security.ShopmeUserDetails;
 import com.shopme.common.entity.User;
 
@@ -23,5 +31,28 @@ public class AccountController {
 		model.addAttribute("user", user);
 		
 		return "account_form";
+	}
+	
+	@PostMapping("/account/update")
+	public String updateDetails(User user, RedirectAttributes redirectAttributes,
+			@RequestParam("image") MultipartFile multipartFile) throws IOException {
+		
+		if (!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			user.setPhotos(fileName);
+			User savedUser = service.updateAccount(user);
+			
+			String uploadDir = "user-photos/" + savedUser.getId();
+			
+			FileUploadUtil.cleanDirectory(uploadDir);
+			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+		} else {
+			if (user.getPhotos().isEmpty()) user.setPhotos(null);
+			service.updateAccount(user);
+		}
+		
+		redirectAttributes.addFlashAttribute("message", "Your account details have been updated!");
+		
+		return "redirect:/account";
 	}
 }
